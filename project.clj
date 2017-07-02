833f487a5248457db20b6fe9f3d9da468dc44646
dmitri.sotnikov@gmail.com <dmitri.sotnikov@gmail.com>
2017-07-02 15:49:27 -0400

    bumped up cljs

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject reagent-frontend/lein-template "0.1.17"
+(defproject reagent-frontend/lein-template "0.1.18"
   :description "A Leiningen template for a ClojureScript setup with Reagent."
   :url "https://github.com/reagent-project/reagent-frontend-template"
   :license {:name "MIT License"
