0b17e7fd8277cb6a4892f030a9d6fcba3dc4dca3
dmitri.sotnikov@gmail.com <dmitri.sotnikov@gmail.com>
2019-01-07 08:01:14 -0500

    bumped up dependencies

--- resources/leiningen/new/reagent_frontend/project.clj
+++ resources/leiningen/new/reagent_frontend/project.clj
@@ -4,12 +4,12 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
 
-  :dependencies [[org.clojure/clojure "1.9.0"]
+  :dependencies [[org.clojure/clojure "1.10.0"]
                  [org.clojure/clojurescript "1.10.439"]
                  [reagent "0.8.1"]]
 
-  :plugins [[lein-cljsbuild "1.1.5"]
-            [lein-figwheel "0.5.16"]]
+  :plugins [[lein-cljsbuild "1.1.7"]
+            [lein-figwheel "0.5.18"]]
 
   :clean-targets ^{:protect false}
   [:target-path
@@ -49,6 +49,6 @@
 
   :profiles {:dev {:source-paths ["src" "env/dev/clj"]
                    :dependencies [[binaryage/devtools "0.9.10"]
-                                  [figwheel-sidecar "0.5.16"]
-                                  [nrepl "0.4.4"]
-                                  [cider/piggieback "0.3.8"]]}})
+                                  [figwheel-sidecar "0.5.18"]
+                                  [nrepl "0.5.3"]
+                                  [cider/piggieback "0.3.10"]]}})
