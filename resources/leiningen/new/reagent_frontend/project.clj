f16db9b3beaf3d2d2a66fafbd5f29bc9137257ac
dmitri.sotnikov@gmail.com <dmitri.sotnikov@gmail.com>
2021-03-09 14:29:35 -0500

    bumped up clojure

--- resources/leiningen/new/reagent_frontend/project.clj
+++ resources/leiningen/new/reagent_frontend/project.clj
@@ -4,7 +4,7 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
 
-  :dependencies [[org.clojure/clojure "1.10.1"]
+  :dependencies [[org.clojure/clojure "1.10.3"]
                  [org.clojure/clojurescript "1.10.773"]
                  [reagent "1.0.0"]]
 
