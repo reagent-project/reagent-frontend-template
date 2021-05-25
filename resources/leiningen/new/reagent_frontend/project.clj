8fb59b49886eb33569c63caf3e29042081b456ab
dmitri.sotnikov@gmail.com <dmitri.sotnikov@gmail.com>
2021-05-25 14:20:38 -0400

    bumped up cljs

--- resources/leiningen/new/reagent_frontend/project.clj
+++ resources/leiningen/new/reagent_frontend/project.clj
@@ -5,7 +5,7 @@
             :url "http://www.eclipse.org/legal/epl-v10.html"}
 
   :dependencies [[org.clojure/clojure "1.10.3"]
-                 [org.clojure/clojurescript "1.10.844"]
+                 [org.clojure/clojurescript "1.10.866"]
                  [reagent "1.0.0"]]
 
   :plugins [[lein-cljsbuild "1.1.7"]
